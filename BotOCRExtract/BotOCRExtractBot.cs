aeee616948c5103738e142063bc1c7c97527815f
Ibrahim KIVANC <ikivanc@hotmail.com>
2019-03-08 21:59:59 +0300

    A method is removed
    A method is removed

--- BotOCRExtract/BotOCRExtractBot.cs
+++ BotOCRExtract/BotOCRExtractBot.cs
@@ -81,8 +81,6 @@ namespace BotOCRExtract
                 var imageAttachment = turnContext.Activity.Attachments?.FirstOrDefault(a => a.ContentType.Contains("image"));
                 if (imageAttachment != null)
                 {
-                   // HandleIncomingAttachment(activity, reply);
-
                     Task.Run(async () =>
                     {
                         var ocrresult = await this.GetCaptionAsync(turnContext.Activity, connector);
@@ -146,28 +144,6 @@ namespace BotOCRExtract
                 },
             };
             await turnContext.SendActivityAsync(reply, cancellationToken);
-        }
-
-
-        private static void HandleIncomingAttachment(IMessageActivity activity, IMessageActivity reply)
-        {
-            foreach (var file in activity.Attachments)
-            {
-                // Determine where the file is hosted.
-                var remoteFileUrl = file.ContentUrl;
-
-                // Save the attachment to the system temp directory.
-                var localFileName = Path.Combine(Path.GetTempPath(), file.Name);
-
-                // Download the actual attachment
-                using (var webClient = new WebClient())
-                {
-                    webClient.DownloadFile(remoteFileUrl, localFileName);
-                }
-
-                reply.Text = $"Attachment \"{activity.Attachments[0].Name}\"" +
-                             $" has been received and saved to \"{localFileName}\"";
-            }
         }
 
 
